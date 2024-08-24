3a675bbb9e9d406633f888621b7fa44aca40263a
Danik-Ux <aimdannewz@gmail.com>
2024-08-24 11:24:01 +0300

    Updated project files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Redirect to AWS Cognito Hosted UI for authentication.
 if (!window.location.href.includes('code=')) {
-    window.location.href = 'https://knowtheearth.auth.us-west-2.amazoncognito.com/login?client_id=14rnop7mqm59es8ku2h5m9vkaa&response_type=code&scope=email+openid+phone&redirect_uri=https://main.d3hwxvxjqggka.amplifyapp.com';
+    window.location.href = 'https://knowtheearth.auth.us-west-2.amazoncognito.com/login?client_id=14rnop7mqm59es8ku2h5m9vkaa&response_type=code&scope=email+openid+phone&redirect_uri=http://know-the-earth-frontend.s3-website-us-west-2.amazonaws.com';
 } else {
     // Assuming the user is authenticated, you can now run your map initialization code
 
